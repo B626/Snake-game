26940eb52de5a51207031c14d3424116bee2a1b1
Bohdan <kovadlobogdan@gmail.com>
2023-05-17 15:59:11 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+class FieldShell{
+   constructor(color) {
+      this.color = color
+   }
+} 
+
+let shell = new FieldShell('#a1ee4a')
+
+class Snake{
+   constructor(color) {
+      this.color = color
+   }
+} 
+
+let snake = new Snake('green')
+
+class Food{
+   constructor(color) {
+      this.color = color
+   }
+}
+
+let food = new Food('red')
